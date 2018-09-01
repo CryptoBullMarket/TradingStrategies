36cdf7102ec03612ab370f77ff98031af9d6c859
shyampandya97@gmail.com <shyampandya97@gmail.com>
2018-09-01 21:35:00 +0530

    Added Double Top Double Bottom stategy

--- handler/connection.py
+++ handler/connection.py
@@ -1,9 +1,25 @@
 from res import constants as constants, id as id
+import pandas as pd
 import requests
 
+params = {
+    'limit' : 50
+}
 
 def get_price_action(_from, _to, time_frame):
     # data is in sorted in ascending order by time
-    data = requests.get(constants.url[id.price_action].format(time_frame, _from, _to)).json()
+    data = requests.get(constants.url[id.price_action].format(time_frame, _from, _to), params=params).json()
+
+    #Converting it into a Dataframe
+    data = pd.DataFrame(data)
+
+    data.columns = [id.time, id.open, id.close, id.high, id.close, id.volume]
+    data = data.sort_values(by=[id.time])
+
     # filter data as needed
     return data
+
+def get_symbol_list():
+    data = requests.get("https://api.bitfinex.com/v1/symbols").json()
+
+    print(data)
--- res/constants.py
+++ res/constants.py
@@ -4,7 +4,7 @@ import os
 
 # url
 url = {
-    id.price_action: 'https://api.bitfinex.com/v2/candles/trade:{}:t{}{}/hist?sort=1',
+    id.price_action: 'https://api.bitfinex.com/v2/candles/trade:{}:t{}{}/hist',
 }
 
 # strategy wise
@@ -13,11 +13,26 @@ three_black_crow = {
     id.wick_percentage: 0.5
 }
 
+double_top_double_bottom = {
+    id.window_size: 14,
+    id.
+}
+
 # database
 database = {
     id.db_url: os.getenv('database'),
 }
 
+coinbase = [
+    "TRX_USD",
+    "BTC_USD",
+    "ETH_USD",
+    "XRP_USD",
+    "ETH_BTC",
+    "XRP_BTC",
+    "TRX_BTC"
+
+            ]
 
 
 
