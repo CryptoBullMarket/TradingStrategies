4d798b18afc2c79d06d0d58004c4dbe3b7b4e838
nischitpra@gmail.com <nischitpra@gmail.com>
2018-09-04 15:40:16 +0530

    key should be upper cased

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from res import constants as constants
 
 def examine_strategies(time_frame):
     for key in constants.coinbase:
+        key=key.upper()
         price_action = connection.get_price_action(key, time_frame)
         tbc.three_black_crows(key, price_action, time_frame)
         dtdb.double_top_double_bottom(key, price_action, time_frame)
