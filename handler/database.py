609acc1a231f69e2a94d6860b3df89e21e27b62f
nischitpra@gmail.com <nischitpra@gmail.com>
2018-09-04 15:18:58 +0530

    insert query column name

--- handler/database.py
+++ handler/database.py
@@ -5,6 +5,6 @@ from res import id as id, constants as constants
 def insert_strategy(key, time_frame, strategy_name, time):
     connection = psycopg2.connect(constants.database[id.db_url])
     cur = connection.cursor()
-    cur.execute("insert into strategy_filter (key,time_frame,strategy_name,time) values ({},{},{},{});"
+    cur.execute("insert into strategy_filter (_key,time_frame,strategy_name,time) values ({},{},{},{});"
                 .format(key, time_frame, strategy_name, time))
     connection.commit()
